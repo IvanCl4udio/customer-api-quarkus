968c15c64d429902e797089301c8b689ed732221
Ivan Cl4udio <ivan.claudio@gmail.com>
2023-10-17 15:32:21 -0300

    Added logger

--- src/main/java/com/ivancl4udio/exception/ConstraintViolationExceptionMapper.java
+++ src/main/java/com/ivancl4udio/exception/ConstraintViolationExceptionMapper.java
@@ -4,15 +4,22 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConstraintViolationExceptionMapper.class);
+
 
     @Override
     public Response toResponse(ConstraintViolationException e) {
         ResponseError erro = new ResponseError();
         erro.setStatus(Response.Status.BAD_REQUEST.getStatusCode());
         erro.setMensagem(e.getMessage());
+
+        LOGGER.error("Violação de constraint detectada", e);
 
         return Response
                 .status(Response.Status.BAD_REQUEST)
